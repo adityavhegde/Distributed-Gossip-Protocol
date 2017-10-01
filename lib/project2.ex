c33467a4a1ceabdf1750f3f0d15fa527a1690800
Aditya Hegde <adityavhegde@gmail.com>
2017-10-01 00:31:02 -0400

    2d topology completed

--- lib/project2.ex
+++ lib/project2.ex
@@ -1,9 +1,43 @@
 defmodule Gossip do
   
-  @doc """"
-
-  """
   def grid2DTopology(nodesList) do
+
+    len = :math.sqrt(tuple_size(nodesList)) |> round
+
+    list2d = Gossip.segment(nodesList, {}, {}, 0, len)
+    
+    Enum.each(0..len-1, fn(i) -> 
+      Enum.each(0..len-1, fn(j) ->
+        neighbors = {}
+        cond do
+          i-1 >= 0 ->
+            neighbors = Tuple.append(neighbors, list2d |> elem(i-1) |> elem(j))
+          true ->true
+        end 
+
+        cond do
+          i+1 < len ->
+            neighbors = Tuple.append(neighbors, list2d |> elem(i+1) |> elem(j))
+          true -> true
+        end 
+
+        cond do
+          j-1 >= 0 ->
+            neighbors = Tuple.append(neighbors, list2d |> elem(i) |> elem(j-1))
+          true -> true
+        end
+
+        cond do
+          j + 1 < len ->
+            neighbors = Tuple.append(neighbors, list2d |> elem(i) |> elem(j+1))
+          true -> true
+        end
+
+        #send
+          send list2d |> elem(i) |> elem(j), neighbors   
+
+      end)
+    end)
     
   end
 
@@ -19,7 +53,21 @@ defmodule Gossip do
   def createProcesses(numProcesses) do
 
   end
-  
+
+  def segment(nodesList, list2d, tempList, index, len) do
+    cond do
+      rem(index+1, len) == 0 ->
+        cond do 
+          index == tuple_size(nodesList) - 1 ->
+            # returns the 2-d tuple after reading the last element
+            Tuple.append(list2d, Tuple.append(tempList, elem(nodesList, index)))
+          true -> 
+            segment(nodesList, Tuple.append(list2d, Tuple.append(tempList, elem(nodesList, index))), {}, index + 1, len)
+        end
+      true ->
+        segment(nodesList, list2d, Tuple.append(tempList, elem(nodesList, index)), index + 1, len)
+      end
+  end
 end
 
 defmodule Project2 do
@@ -33,25 +81,25 @@ defmodule Project2 do
               |> Enum.at(1)
 
     case topology do
-      full - >
-        createProcesses |> fullTopology
-        
-      2D ->
+      "full" ->
+        Gossip.createProcesses(numNodes) |> Gossip.fullTopology
+
+      "2D" ->
         :math.sqrt(numNodes) 
           |> Float.round(0)
           |> :math.pow(2)
-          |> createProcesses 
-          |> grid2DTopology
+          |> Gossip.createProcesses(numNodes)
+          |> Gossip.grid2DTopology
       
-      line ->
-        createProcesses |> lineTopology
+      "line" ->
+        Gossip.createProcesses(numNodes) |> Gossip.lineTopology
         
-      imp2D ->
+      "imp2D" ->
         :math.sqrt(numNodes) 
         |> Float.round(0)
         |> :math.pow(2)
-        |> createProcesses 
-        |> gridImpTopology
+        |> Gossip.createProcesses(numNodes) 
+        |> Gossip.gridImpTopology
     
     end
   end
